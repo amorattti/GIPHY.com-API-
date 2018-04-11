e95194a3c147ddc22d9577caad7d310276992c8a
Tobiasz18 <tobiaszpost@gmail.com>
2018-04-11 20:38:13 +0200

    app component/ fetch request

--- components/App.js
+++ components/App.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import style from './style.scss';
+import styles from './style.scss';
+const API_GIPHY = 'aUhPqNQX1V9W5wSNJ9J0FOaQdvwgHH7O';
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            loader : false,
+            searchText : '',
+            gif : {}
+        };
+        this.searchGif();
+    }
+
+    searchGif() {
+        this.setState({
+            loader: true
+        });
+        this.getUrl('cat', gif => { // callback(myJson)
+            const getDetails = {
+                image: gif.data.fixed_height_downsampled_url,
+                title:  gif.data.title,
+                gifUrl: gif.data.url
+            }
+            this.setState({
+                loader : true,
+                searchText: 'cat',
+                gif : getDetails
+            });
+        })
+    }
+
+    getUrl(searchText, callback) {
+        const url = `http://api.giphy.com/v1/gifs/random?&tag=${searchText}&api_key=${API_GIPHY}`
+        fetch(url)
+        .then(response => response.json())
+        .then(myJson => callback(myJson))
+    }
+
     render() {
+        console.log('tu state', this.state)
         return (
-            <div style="style.body">
-                <h1>Hellow World</h1>
+            <div style={styles}>
+                <h1>The GIF Search </h1>
             </div>
         );
     }
