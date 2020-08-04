ae2676dd71dfc356c41cebeabc265698d2d9cb92
Tobiasz Łoś <tobiaszpost@gmail.com>
2020-08-04 22:20:54 +0200

    tab space fix

--- src/api/fetch.gif.js
+++ src/api/fetch.gif.js
@@ -4,23 +4,23 @@ const URL = 'http://api.giphy.com/v1/gifs/';
 const KEY = 'aUhPqNQX1V9W5wSNJ9J0FOaQdvwgHH7O';
 
 export const fetchRandomGif = async (name) => {
-	const { data } = await axios.get(URL + 'random', {
-		params: {
-			tag: name,
-			api_key: KEY
-		}
-	});
+  const { data } = await axios.get(URL + 'random', {
+    params: {
+      tag: name,
+      api_key: KEY
+    }
+   });
 
-	return data;
+  return data;
 };
 
 export const fetchTrendingGifs = async () => {
-	const { data } = await axios.get(URL + 'trending', {
-		params: {
-			limit: 4,
-			api_key: KEY
-		}
-	});
+  const { data } = await axios.get(URL + 'trending', {
+    params: {
+      limit: 4,
+      api_key: KEY
+    }
+  });
 
-	return data
+  return data
 };
